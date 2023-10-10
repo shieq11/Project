74b76d4f01f9a6c3f67cf4eb684cb095f4c19266
Mateusz Iwan <143963948+shieq11@users.noreply.github.com>
2023-10-10 19:08:24 +0200

    zgadywanie liczb

---
+++ numbers_guess/numbers_guess.py
@@ -0,0 +1,37 @@
+# Zagadywanie liczb wybieranych losowo
+
+import random
+
+top_of_range = input("Typ a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please typ a number larger than a 0")
+        quit()
+else:
+    print("Please type a number next time")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a quess: ")
+    if user_guess.isdigit():
+         user_guess = int(user_guess)
+    else:
+        print("Please type a number next time")
+        continue
+
+    if user_guess == random_number:
+        print("You got it")
+        break
+    elif user_guess >random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+
+print("You got it in", guesses, "guesses")
